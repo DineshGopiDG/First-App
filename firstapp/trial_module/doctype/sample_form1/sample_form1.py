d689e39e7ca4a97fcc12fd68b8e3a60010104605
dinesh gopi <alagupandian.k@doodleblue.com>
2024-01-23 12:40:59 +0530

    first commit

---
+++ firstapp/trial_module/doctype/sample_form1/sample_form1.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MRR and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SampleForm1(Document):
+	pass
